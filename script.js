59cf4d609d68e381bea5f66d92c853ae7bc6a1ca
Sibu07 <91250474+Sibu07@users.noreply.github.com>
2023-07-30 08:36:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function convertToRaw() {
+  const githubLink = document.getElementById('githubLink').value;
+  const rawLink = githubLink.replace('github.com', 'raw.githubusercontent.com');
+  document.getElementById('rawLink').innerText = rawLink;
+  document.getElementById('copyBtn').classList.remove('hide');
+}
+
+function copyToClipboard() {
+  const rawLink = document.getElementById('rawLink').innerText;
+  const textarea = document.createElement('textarea');
+  textarea.value = rawLink;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  alert('Copied to clipboard!\n\n' + rawLink);
+}
