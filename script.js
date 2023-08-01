5c4743ec6233dafc2437d2254785875481e3206a
Sibu07 <91250474+Sibu07@users.noreply.github.com>
2023-08-01 21:23:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
 function convertToRaw() {
-  const githubLink = document.getElementById('githubLink').value;
-  let rawLink = githubLink.replace('github.com', 'raw.githubusercontent.com');
-  rawLink = rawLink.replace('/blob', ''); // Remove '/blob' from the link if present
-  document.getElementById('rawLink').innerText = rawLink;
-  document.getElementById('copyBtn').classList.remove('hide');
+  const inputLink = document.getElementById("githubLink").value.trim();
+  if (inputLink === "") {
+    return;
+  }
+
+  const rawLink = getRawLink(inputLink);
+  displayRawLink(rawLink);
+
+  // Show the Clear Input button and hide the Convert button
+  document.getElementById("clearBtn").classList.remove("hide");
+  document.getElementById("convertBtn").classList.add("hide");
 }
 
-function copyToClipboard() {
-  const rawLink = document.getElementById('rawLink').innerText;
-  const textarea = document.createElement('textarea');
-  textarea.value = rawLink;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy');
-  document.body.removeChild(textarea);
-  alert('Copied to clipboard!\n\n' + rawLink);
+function getRawLink(link) {
+  // Check if the link contains '/blob'
+  const blobIndex = link.indexOf('/blob');
+  if (blobIndex !== -1) {
+    // Remove '/blob' from the link
+    link = link.slice(0, blobIndex) + link.slice(blobIndex + 5);
+  }
+
+  // Check if the link ends with '/'
+  if (link.endsWith('/')) {
+    // Remove the trailing '/'
+    link = link.slice(0, -1);
+  }
+
+  // Convert the GitHub URL to raw content URL
+  const rawLink = link.replace('github.com', 'raw.githubusercontent.com');
+  return rawLink;
+}
+
+function displayRawLink(rawLink) {
+  document.getElementById("rawLinkText").textContent = rawLink;
+  document.getElementById("rawLink").classList.remove("hide");
+  document.getElementById("copyBtn").classList.remove("hide");
+}
+
+function copyRawLink() {
+  const rawLinkText = document.getElementById("rawLinkText");
+  const textArea = document.createElement("textarea");
+  textArea.value = rawLinkText.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
 }
 
 function clearInput() {
-  document.getElementById('githubLink').value = '';
-  document.getElementById('rawLink').innerText = '';
-  document.getElementById('copyBtn').classList.add('hide');
+  document.getElementById("githubLink").value = "";
+  document.getElementById("rawLink").classList.add("hide");
+  document.getElementById("copyBtn").classList.add("hide");
+  
+  // Hide the Clear Input button and show the Convert button
+  document.getElementById("clearBtn").classList.add("hide");
+  document.getElementById("convertBtn").classList.remove("hide");
 }
